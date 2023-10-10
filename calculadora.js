f876810736bdf85d5bd24d2df0da6b1a713020d2
Javier García <javiergarciarodriguez@alumnos.tuniversformacion.es>
2023-10-10 11:49:29 +0200

    calculadora terminada

---
+++ calculadora.js
@@ -0,0 +1,35 @@
+function addToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+function addSquareRoot() {
+    const currentValue = document.getElementById('display').value;
+    try {
+        const result = Math.sqrt(eval(currentValue));
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+function addLogarithm() {
+    const currentValue = document.getElementById('display').value;
+    try {
+        const result = Math.log10(eval(currentValue));
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
